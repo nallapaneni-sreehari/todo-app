312c01a601ecb059a1c965a9bb7fb444fbf51a7f
r151149 <45209359+r151149@users.noreply.github.com>
2023-11-24 21:39:37 +0530

    added screen reduce

--- src/components/tasks/ListTask.jsx
+++ src/components/tasks/ListTask.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function ListTask() {
   return (
-    <div>ListTask</div>
-  )
+    <div className="p-5">
+      <div className="flex flex-col gap-5">
+        <h1 className="text-3xl font-[600]">Your Tasks</h1>
+      </div>
+    </div>
+  );
 }
 
-export default ListTask
+export default ListTask;
